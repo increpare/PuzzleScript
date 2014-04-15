283ad0363bab09c07dcfa6a7d99ffc651b07328e
mechturk <mechturk@gmail.com>
2014-04-16 09:23:38 +1000

    Implemented resizable panels using jquery-ui
    All of the resizing happens in layout.js.
    Layout.css has also been modified as jquery-ui-resizable needs relative
    position to work.

---
+++ js/layout.js
@@ -0,0 +1,39 @@
+
+function resize_righthalf(event, ui){
+	newwidth = $("BODY").width() - ui.size["width"];
+	bottomhalfheight = $("BODY").height() - (ui.position["top"] + ui.size["height"]);
+	$( ".righttophalf").width(newwidth);
+	$( ".rightbottomhalf" ).width(newwidth);
+	canvasResize();
+};
+
+function resize_rightbottom(event, ui){
+	bottomhalfheight = $("BODY").height() - (ui.position["top"] + ui.size["height"]);
+	$( ".rightbottomhalf" ).height(bottomhalfheight);
+	$( ".leftpanel" ).width( $("BODY").width() - ui.size["width"]);
+	canvasResize();
+}
+
+function resize_all(){
+	newwidth = $("BODY").width() - $(".leftpanel").width();
+	$( ".righttophalf").width(newwidth);
+	$( ".rightbottomhalf" ).width(newwidth);
+	canvasResize();
+	
+	bottomhalfheight = $("BODY").height() - ($(".righttophalf").position()["top"] + $(".righttophalf").height());
+	$( ".rightbottomhalf" ).height(bottomhalfheight);
+};
+
+$( ".leftpanel" ).resizable({
+	handles: "e",
+	resize: resize_righthalf,
+	stop: resize_righthalf
+});
+
+$( ".righttophalf" ).resizable({
+	handles: "s",
+	resize: resize_rightbottom,
+	stop: resize_rightbottom
+} );
+
+window.onresize = resize_all;
