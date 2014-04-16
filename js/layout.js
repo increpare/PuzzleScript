f89a37c34800f46aa795d6946aa6395b60f2b995
mechturk <mechturk@gmail.com>
2014-04-16 19:21:01 +1000

    Improved resizing behaviour on quick drag to max
    Also refactored resizing into functions

--- js/layout.js
+++ js/layout.js
@@ -2,6 +2,38 @@ var soundbarwidth = 440;
 var lowerbarheight = 18;
 var upperbarheight = 30;
 
+function resize_widths(verticaldragbarX){
+	document.getElementById("leftpanel").style.width = verticaldragbarX + "px";
+	document.getElementById("righttophalf").style.left = verticaldragbarX + 3 + "px";
+	document.getElementById("rightbottomhalf").style.left = verticaldragbarX + 3 + "px";
+	document.getElementById("horizontaldragbar").style.left = verticaldragbarX + 3 + "px";
+	document.getElementById("verticaldragbar").style.left = verticaldragbarX + "px";
+	canvasResize();
+}
+
+function resize_heights(horizontaldragbarY){
+	document.getElementById("leftpanel").style.height = (window.innerHeight - 30) + "px";
+	document.getElementById("verticaldragbar").style.height = (window.innerHeight - 30) + "px";
+	document.getElementById("righttophalf").style.height = horizontaldragbarY - upperbarheight + "px";
+	document.getElementById("rightbottomhalf").style.top = horizontaldragbarY + 3 + "px";
+	document.getElementById("horizontaldragbar").style.top = horizontaldragbarY + "px";
+	canvasResize();
+}
+
+function resize_all(){
+	verticaldragbarX = parseInt(document.getElementById("verticaldragbar").style.left.replace("px",""));
+	if ((window.innerWidth - verticaldragbarX) < soundbarwidth){
+		verticaldragbarX = window.innerWidth - soundbarwidth - 30;
+	};
+	resize_widths(verticaldragbarX);
+	
+	horizontaldragbarY = parseInt(document.getElementById("horizontaldragbar").style.top.replace("px",""));
+	if ((window.innerHeight - horizontaldragbarY) < (lowerbarheight)){
+		horizontaldragbarY = window.innerHeight - lowerbarheight - 7;
+	};
+	resize_heights(horizontaldragbarY);
+};
+
 function verticalDragbarMouseDown(e) {
     e.preventDefault();
     window.addEventListener("mousemove", verticalDragbarMouseMove, false);
@@ -10,17 +42,13 @@ function verticalDragbarMouseDown(e) {
 
 function verticalDragbarMouseMove(e) {
 	if ((window.innerWidth - e.pageX) > soundbarwidth){
-		document.getElementById("leftpanel").style.width = e.pageX + 2 + "px";
-		document.getElementById("righttophalf").style.left = e.pageX + 5 + "px";
-		document.getElementById("rightbottomhalf").style.left = e.pageX + 5 + "px";
-		document.getElementById("horizontaldragbar").style.left = e.pageX + 5 + "px";
-		document.getElementById("verticaldragbar").style.left = e.pageX + 2 + "px";
-		canvasResize();
+		resize_widths(e.pageX + 2);
+	} else {
+		resize_widths(window.innerWidth - soundbarwidth - 30);
 	};
 };
 
 function verticalDragbarMouseUp(e) {
-    //document.getElementById("clickevent").innerHTML = 'in another mouseUp event' + i++;
     window.removeEventListener("mousemove", verticalDragbarMouseMove, false);
 };
 
@@ -31,55 +59,18 @@ function horizontalDragbarMouseDown(e) {
 };
 
 function horizontalDragbarMouseMove(e) {
-	if ((window.innerHeight - e.pageY) > (lowerbarheight + 8)){
-		document.getElementById("righttophalf").style.height = e.pageY + 2 - upperbarheight + "px";
-		document.getElementById("rightbottomhalf").style.top = e.pageY + 5 + "px";
-		document.getElementById("horizontaldragbar").style.top = e.pageY + 2 + "px";
-		canvasResize();
+	if ((window.innerHeight - e.pageY) > (lowerbarheight + 7)){
+		resize_heights(e.pageY + 2);
+	} else {
+		resize_heights(window.innerHeight - lowerbarheight - 7);
 	}
 };
 
 function horizontalDragbarMouseUp(e) {
-    //document.getElementById("clickevent").innerHTML = 'in another mouseUp event' + i++;
     window.removeEventListener("mousemove", horizontalDragbarMouseMove, false);
 };
 
-function resize_all(){
-	document.getElementById("leftpanel").style.height = (window.innerHeight - 30) + "px";
-	document.getElementById("verticaldragbar").style.height = (window.innerHeight - 30) + "px";
-
-	verticaldragbarX = parseInt(document.getElementById("verticaldragbar").style.left.replace("px",""));
-	
-	if ((window.innerWidth - verticaldragbarX) < soundbarwidth){
-		verticaldragbarX = window.innerWidth - soundbarwidth - 30;
-	};
-	
-	document.getElementById("leftpanel").style.width = verticaldragbarX + "px";
-	document.getElementById("righttophalf").style.left = verticaldragbarX + 3 + "px";
-	document.getElementById("rightbottomhalf").style.left = verticaldragbarX + 3 + "px";
-	document.getElementById("horizontaldragbar").style.left = verticaldragbarX + 3 + "px";
-	document.getElementById("verticaldragbar").style.left = verticaldragbarX + "px";
-	
-	horizontaldragbarY = parseInt(document.getElementById("horizontaldragbar").style.top.replace("px",""));
-	if ((window.innerHeight - horizontaldragbarY) < (lowerbarheight)){
-		horizontaldragbarY = window.innerHeight - lowerbarheight - 8;
-	};
-	
-	document.getElementById("righttophalf").style.height = horizontaldragbarY - upperbarheight + "px";
-	document.getElementById("rightbottomhalf").style.top = horizontaldragbarY + 3 + "px";
-	document.getElementById("horizontaldragbar").style.top = horizontaldragbarY + "px";
-	canvasResize();
-};
-
 function adjust_panels(){
-	document.getElementById("leftpanel").style.height = (window.innerHeight - 30) + "px";
-	document.getElementById("verticaldragbar").style.height = (window.innerHeight - 30) + "px";
-	document.getElementById("righttophalf").style.left = (window.innerWidth/2 + 3) + "px";
-	document.getElementById("rightbottomhalf").style.left = (window.innerWidth/2 + 3) + "px";
-	document.getElementById("rightbottomhalf").style.top = (window.innerHeight/2 + 3) + "px";
-	document.getElementById("horizontaldragbar").style.left = (window.innerWidth/2 + 3) + "px";
-	
-	//need to have these in px in case resize_all is called
-	document.getElementById("verticaldragbar").style.left = (window.innerWidth/2) + "px";
-	document.getElementById("horizontaldragbar").style.top = (window.innerHeight/2) + "px";
+	resize_widths(window.innerWidth/2);
+	resize_heights(window.innerHeight/2);
 };
