2219353fdf7f8a2e8d9825295d113fb6d5b49718
mechturk <mechturk@gmail.com>
2014-04-16 12:52:33 +1000

    Set limits on resizing to ensure no breakage
    Addresses problems with resizing things too small/too far.

--- js/layout.js
+++ js/layout.js
@@ -1,7 +1,11 @@
+var soundbarwidth = 440;
 
 function resize_righthalf(event, ui){
+	if ($("BODY").width() - ui.size["width"] <= soundbarwidth){
+		return;
+	};
+	
 	newwidth = $("BODY").width() - ui.size["width"];
-	bottomhalfheight = $("BODY").height() - (ui.position["top"] + ui.size["height"]);
 	$( ".righttophalf").width(newwidth);
 	$( ".rightbottomhalf" ).width(newwidth);
 	canvasResize();
@@ -10,31 +14,50 @@ function resize_righthalf(event, ui){
 function resize_rightbottom(event, ui){
 	bottomhalfheight = $("BODY").height() - (ui.position["top"] + ui.size["height"]);
 	$( ".rightbottomhalf" ).height(bottomhalfheight);
-	$( ".leftpanel" ).width( $("BODY").width() - ui.size["width"]);
+	
 	canvasResize();
 }
 
 function resize_all(){
-	$(".leftpanel").height($("BODY").height() - $(".uppertoolbar").height());
-	newwidth = $("BODY").width() - $(".leftpanel").width();
-	$( ".righttophalf").width(newwidth);
-	$( ".rightbottomhalf" ).width(newwidth);
-	canvasResize();
+	maxtophalfheight = ($("BODY").height()-($(".uppertoolbar").height() + $(".lowertoolbar").height() + 2));
+
+	//reset maximums drag extents
+	$( ".righttophalf" ).resizable( "option", "maxHeight", maxtophalfheight);
+	$(".leftpanel").resizable( "option", "maxWidth", ($("BODY").width() - soundbarwidth));
 	
-	bottomhalfheight = $("BODY").height() - ($(".righttophalf").position()["top"] + $(".righttophalf").height());
+	//resize panel heights
+	if ($(".righttophalf").height() > maxtophalfheight){
+		$(".righttophalf").height(maxtophalfheight);
+		bottomhalfheight = $("BODY").height() - (maxtophalfheight);
+	} else {
+		bottomhalfheight = $("BODY").height() - ($(".uppertoolbar").height() + $(".righttophalf").height());
+	};
 	$( ".rightbottomhalf" ).height(bottomhalfheight);
+	$(".leftpanel").height($("BODY").height() - $(".uppertoolbar").height());
+	
+	//resize panel widths
+	if ($("BODY").width() - $(".leftpanel").width() < soundbarwidth){
+		$(".leftpanel").width($("BODY").width() - soundbarwidth);
+	};
+	RHSwidth = $("BODY").width() - $(".leftpanel").width();
+	$( ".righttophalf").width(RHSwidth);
+	$( ".rightbottomhalf" ).width(RHSwidth);
+	
+	canvasResize();
 };
 
 $( ".leftpanel" ).resizable({
 	handles: "e",
 	resize: resize_righthalf,
-	stop: resize_righthalf
+	stop: resize_righthalf,
+	maxWidth: ($("BODY").width() - soundbarwidth)
 });
 
 $( ".righttophalf" ).resizable({
 	handles: "s",
 	resize: resize_rightbottom,
-	stop: resize_rightbottom
+	stop: resize_rightbottom,
+	maxHeight: ($("BODY").height()-($(".uppertoolbar").height() + $(".lowertoolbar").height() + 2))
 } );
 
 window.onresize = resize_all;
