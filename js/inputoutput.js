dea0e1f5f0bdb19466e295bb0d18b4adc629e94a
Stephen Lavelle <analytic@gmail.com>
2017-04-30 09:31:01 +0100

    dunno - looks harmless?

--- js/inputoutput.js
+++ js/inputoutput.js
@@ -374,7 +374,7 @@ function onKeyDown(event) {
     	return;
     }
 
-    if(lastDownTarget === canvas) {
+    if(lastDownTarget === canvas || (window.Mobile && (lastDownTarget === window.Mobile.focusIndicator) ) ){
     	if (keybuffer.indexOf(event.keyCode)===-1) {
     		keybuffer.splice(keyRepeatIndex,0,event.keyCode);
 	    	keyRepeatTimer=0;
