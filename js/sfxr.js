f15d9c6e288a85b003bc50b062c9a3e4abb3714d
Stephen Lavelle <analytic@gmail.com>
2018-12-14 10:54:23 +0100

    get audio working on chrome and safari

--- js/sfxr.js
+++ js/sfxr.js
@@ -607,7 +607,35 @@ SoundEffect.prototype.getBuffer = function() {
 };
 
 
+//unlock bullshit
+function ULBS(){   
+  if (AUDIO_CONTEXT.state === 'suspended')
+  {
+      var unlock = function()
+      {
+        AUDIO_CONTEXT.resume().then(function()
+          {
+            document.body.removeEventListener('touchstart', unlock);
+            document.body.removeEventListener('touchend', unlock);
+            document.body.removeEventListener('mousedown', unlock);
+            document.body.removeEventListener('mouseup', unlock);
+            document.body.removeEventListener('keydown', unlock);
+            document.body.removeEventListener('keyup', unlock);
+          });
+      };
+
+      document.body.addEventListener('touchstart', unlock, false);
+      document.body.addEventListener('touchend', unlock, false);
+      document.body.addEventListener('mousedown', unlock, false);
+      document.body.addEventListener('mouseup', unlock, false);
+      document.body.addEventListener('keydown', unlock, false);
+      document.body.addEventListener('keyup', unlock, false);
+  }
+}
+
 SoundEffect.prototype.play = function() {
+  ULBS();
+
   var source = AUDIO_CONTEXT.createBufferSource();
   var filter1 = AUDIO_CONTEXT.createBiquadFilter();
   var filter2 = AUDIO_CONTEXT.createBiquadFilter();
@@ -982,6 +1010,7 @@ function cacheSeed(seed){
 
   return sound;
 }
+
 
 function playSound(seed) {
   if (muted){
