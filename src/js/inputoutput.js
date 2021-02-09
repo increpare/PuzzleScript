3a23c745460e6e119960f47cbe5c204e4cea3728
Stephen Lavelle <analytic@gmail.com>
2021-02-09 10:38:55 +0100

    fixes #558

--- src/js/inputoutput.js
+++ src/js/inputoutput.js
@@ -393,7 +393,7 @@ function onKeyDown(event) {
 
 	// Prevent arrows/space from scrolling page
 	if ((!IDE) && ([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1)) {
-		if (e&&(e.ctrlKey || e.metaKey)){
+		if (event&&(event.ctrlKey || event.metaKey)){
 		} else {
 			prevent(event);
 		}
