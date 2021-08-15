cf4f189a6377e65a6f803a525cdea95b5233704d
Stephen Lavelle <analytic@gmail.com>
2021-08-15 21:47:30 +0200

    fixes #747

--- src/js/graphics.js
+++ src/js/graphics.js
@@ -112,12 +112,21 @@ function generateGlyphImages() {
 	glyphImagesCorrespondance=[];
 	glyphImages=[];
 	
+    seenobjects = {};
 	for (var n in state.glyphDict) {
-		if (n.length==1 && state.glyphDict.hasOwnProperty(n)) {
+		if (n.length==1 && state.glyphDict.hasOwnProperty(n)) {            
 			var g=state.glyphDict[n];
 			var sprite = makeSpriteCanvas("C"+n)
 			var spritectx = sprite.getContext('2d');
 			glyphImagesCorrespondance.push(n);
+
+            /* hide duplicate entries from editor palette*/
+            var trace = g.join(",");
+            if (seenobjects.hasOwnProperty(trace)){
+                continue;
+            }
+            seenobjects[trace]=true;
+
 			for (var i=0;i<g.length;i++){
 				var id = g[i];
 				if (id===-1) {
