increpare/PuzzleScript

commit 0edceb98e8b28e21f97d80235c4277f697cf2e40

better checks for dimensions in flickscreen+zoomscreen

src/js/compiler.js
@@ -2177,6 +2177,10 @@ function checkObjectsAreLayered(state) {
     }
 }
 
+function isInt(value) {
+return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
+}
+
 function twiddleMetaData(state) {
     var newmetadata = {};
     for (var i = 0; i < state.metadata.length; i += 2) {
@@ -2185,17 +2189,35 @@ function twiddleMetaData(state) {
         newmetadata[key] = val;
     }
 
-    var getCoords = function(str,lineNumber){
+
+    const getIntCheckedPositive = function(s,lineNumber){
+        if (!isFinite(s) || !isInt(s)){
+            logWarning(`Wasn't able to make sense of "${s}" as a (whole number) dimension.`,lineNumber);
+            return NaN;
+        }
+        var result = parseInt(s);
+        if (isNaN(result)){
+            logWarning(`Wasn't able to make sense of "${s}" as an dimension.`,lineNumber);
+        }
+        if (result<=0){
+            logWarning(`The dimension given to me (you gave "${s}") is baad - it should be greater than 0.`,lineNumber);
+        }
+        return result;
+    }
+    const getCoords = function(str,lineNumber){
         var coords = val.split('x');
         if (coords.length!==2){
             logWarning("Dimensions must be of the form AxB.",lineNumber);
             return null;
         } else {
-            var intcoords = [parseInt(coords[0],10), parseInt(coords[1],10)];
+            var intcoords = [getIntCheckedPositive(coords[0],lineNumber), getIntCheckedPositive(coords[0],lineNumber)];
             if (!isFinite(coords[0]) || !isFinite(coords[1]) || isNaN(intcoords[0]) || isNaN(intcoords[1])) {
                 logWarning(`Couldn't understand the dimensions given to me (you gave "${val}") - should be of the form AxB.`,lineNumber);
                 return null
             } else {
+                if (intcoords[0]<=0 || intcoords[1]<=0){
+                    logWarning(`The dimensions given to me (you gave "${val}") are baad - they should be > 0.`,lineNumber);
+                }
                 return intcoords;
             }
         }

src/tests/resources/errormessage_testdata.js
@@ -843,4 +843,12 @@ var errormessage_testdata = [
     "flickscreen/zoomscreen formatting error #2",
     ["title flickscreen/zoomscreen formatting error #2\nauthor David Skinner\nhomepage www.puzzlescript.net\n\nzoomscreen 123\nflickscreen 123\n\n========\nOBJECTS\n========\n\nBackground\nlightgreen green\n11111\n01111\n11101\n11111\n10111\n\nTarget\ndarkblue\n.....\n.000.\n.0.0.\n.000.\n.....\n\nWall\nbrown darkbrown\n00010\n11111\n01000\n11111\n00010\n\nPlayer\nblack orange white blue\n.000.\n.111.\n22222\n.333.\n.3.3.\n\nCrate\norange\n00000\n0...0\n0...0\n0...0\n00000\n\n=======\nLEGEND\n=======\n\n. = Background\n# = Wall\nP = Player\n* = Crate\n@ = Crate and Target\nO = Target\n\n=======\nSOUNDS\n=======\n\nCrate move 36772507\n\n================\nCOLLISIONLAYERS\n================\n\nBackground\nTarget\nPlayer, Wall, Crate\n\n======\nRULES\n======\n\n[ > Player | Crate ] -> [ > Player | > Crate ] \n\n==============\nWINCONDITIONS\n==============\n\nall Target on Crate\n\n=======\nLEVELS\n=======\n\n####..\n#.O#..\n#..###\n#@P..#\n#..*.#\n#..###\n####..\n\n######\n#....#\n#.#P.#\n#.*@.#\n#.O@.#\n#....#\n######\n\n",["line 6 : Dimensions must be of the form AxB.","line 5 : Dimensions must be of the form AxB."],0]
     ],
+    [
+    "Dimensions should be positive",
+    ["title Dimensions should be positive\nauthor David Skinner\nhomepage www.puzzlescript.net\n\nflickscreen -1x-3\n\n========\nOBJECTS\n========\n\nBackground\nlightgreen green\n11111\n01111\n11101\n11111\n10111\n\nTarget\ndarkblue\n.....\n.000.\n.0.0.\n.000.\n.....\n\nWall\nbrown darkbrown\n00010\n11111\n01000\n11111\n00010\n\nPlayer\nblack orange white blue\n.000.\n.111.\n22222\n.333.\n.3.3.\n\nCrate\norange\n00000\n0...0\n0...0\n0...0\n00000\n\n=======\nLEGEND\n=======\n\n. = Background\n# = Wall\nP = Player\n* = Crate\n@ = Crate and Target\nO = Target\n\n=======\nSOUNDS\n=======\n\nCrate move 36772507\n\n================\nCOLLISIONLAYERS\n================\n\nBackground\nTarget\nPlayer, Wall, Crate\n\n======\nRULES\n======\n\n[ > Player | Crate ] -> [ > Player | > Crate ] \n\n==============\nWINCONDITIONS\n==============\n\nall Target on Crate\n\n=======\nLEVELS\n=======\n\n####..\n#.O#..\n#..###\n#@P..#\n#..*.#\n#..###\n####..\n\n######\n#....#\n#.#P.#\n#.*@.#\n#.O@.#\n#....#\n######\n\n",["line 5 : The dimensions given to me (you gave \"-1x-3\") are baad - they should be > 0."],0]
+    ],
+    [
+    "Dimensions should be positive and integral",
+    ["title Dimensions should be positive and integral\nauthor David Skinner\nhomepage www.puzzlescript.net\n\nzoomscreen 0x4.23\nflickscreen 1.4x-4\n\n========\nOBJECTS\n========\n\nBackground\nlightgreen green\n11111\n01111\n11101\n11111\n10111\n\nTarget\ndarkblue\n.....\n.000.\n.0.0.\n.000.\n.....\n\nWall\nbrown darkbrown\n00010\n11111\n01000\n11111\n00010\n\nPlayer\nblack orange white blue\n.000.\n.111.\n22222\n.333.\n.3.3.\n\nCrate\norange\n00000\n0...0\n0...0\n0...0\n00000\n\n=======\nLEGEND\n=======\n\n. = Background\n# = Wall\nP = Player\n* = Crate\n@ = Crate and Target\nO = Target\n\n=======\nSOUNDS\n=======\n\nCrate move 36772507\n\n================\nCOLLISIONLAYERS\n================\n\nBackground\nTarget\nPlayer, Wall, Crate\n\n======\nRULES\n======\n\n[ > Player | Crate ] -> [ > Player | > Crate ] \n\n==============\nWINCONDITIONS\n==============\n\nall Target on Crate\n\n=======\nLEVELS\n=======\n\n####..\n#.O#..\n#..###\n#@P..#\n#..*.#\n#..###\n####..\n\n######\n#....#\n#.#P.#\n#.*@.#\n#.O@.#\n#....#\n######\n\n",["line 6 : Wasn't able to make sense of \"1.4\" as a (whole number) dimension.","line 6 : Couldn't understand the dimensions given to me (you gave \"1.4x-4\") - should be of the form AxB.","line 5 : The dimension given to me (you gave \"0\") is baad - it should be greater than 0.","line 5 : The dimensions given to me (you gave \"0x4.23\") are baad - they should be > 0."],0]
+    ],
 ];    

commit d2bfdc3e3280d9ad30c3c57b72d60fd7426d313d

Fix non-square flickscreen/zoomscreen coordinates
Regressed in 0edceb98e8b28e21f97d80235c4277f697cf2e40

src/js/compiler.js
@@ -2234,7 +2234,7 @@ function twiddleMetaData(state) {
             logWarning("Dimensions must be of the form AxB.",lineNumber);
             return null;
         } else {
-            var intcoords = [getIntCheckedPositive(coords[0],lineNumber), getIntCheckedPositive(coords[0],lineNumber)];
+            var intcoords = [getIntCheckedPositive(coords[0],lineNumber), getIntCheckedPositive(coords[1],lineNumber)];
             if (!isFinite(coords[0]) || !isFinite(coords[1]) || isNaN(intcoords[0]) || isNaN(intcoords[1])) {
                 logWarning(`Couldn't understand the dimensions given to me (you gave "${val}") - should be of the form AxB.`,lineNumber);
                 return null