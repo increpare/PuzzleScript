904f2b48afc91210acca4db88c826f0997e87826
Stephen Lavelle <analytic@gmail.com>
2024-05-02 17:34:32 +0200

    fixed link to gallery #1012

--- src/games_dat.js
+++ src/games_dat.js
@@ -131,9 +131,9 @@ var games_gallery = [
     "homepage": "zarat.us"
   },
   {
-    "url": "https://www.puzzlescript.net/play.html?p=6893137",
+    "url": "https://www.puzzlescript.net/play.html?p=7072266",
     "thumb": "6893137.gif",
-    "title": "tiny treasure hunt 1.0",
+    "title": "tiny treasure hunt",
     "author": "Weeble",
     "homepage": "www.twitter.com/weeble"
   },
