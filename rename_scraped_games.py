35f0f47afdda90e081605d4796226752624ceb38
Sam Earle <smearle93@gmail.com>
2025-02-06 03:05:09 -0500

    more renaming

--- rename_scraped_games.py
+++ rename_scraped_games.py
@@ -4,6 +4,7 @@ import shutil
 
 files = glob.glob('data/scraped_games/*')
 for file in files:
-    new_filename = re.sub("""([?|'|:|*|\(|\)"|,])""", "_", file)
+    new_filename = re.sub("""([?|'|:|+|~|{|}|*|\(|\)"|,| |\]|\[])""", "_", file)
     shutil.move(file, new_filename)
-    print(f"Renamed {file} to {new_filename}")
+    if new_filename != file:
+        print(f"Renamed {file} to {new_filename}")
