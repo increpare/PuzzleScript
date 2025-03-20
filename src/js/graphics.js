increpare/PuzzleScript

commit 17418c5339f1f1b56290e1a5dff610a415ca56a5

fix graphics broken

src/js/graphics.js
@@ -20,10 +20,14 @@ function createSprite(name,spritegrid, colors, padding) {
     for (var j = 0; j < h; j++) {
         for (var k = 0; k < w; k++) {
             var val = spritegrid[j][k];
-            if (val > 0) {
+            if (val >= 0) {
                 var cy = (j * ch)|0;
                 var cx = (k * cw)|0;
-                spritectx.fillStyle = colors[val];
+                const color = colors[val];
+                if (color==state.bgcolor) {
+                    continue;
+                }
+                spritectx.fillStyle = color;                
                 spritectx.fillRect(cx, cy, cw, pixh);
             }
         }

commit 76a199522ee253debdd17d14b446051d67c17f96

fixed bug with sprites not drawing fully
broken in https://github.com/increpare/PuzzleScript/commit/17418c5339f1f1b56290e1a5dff610a415ca56a5

src/js/graphics.js
@@ -24,9 +24,6 @@ function createSprite(name,spritegrid, colors, padding) {
                 var cy = (j * ch)|0;
                 var cx = (k * cw)|0;
                 const color = colors[val];
-                if (color==state.bgcolor) {
-                    continue;
-                }
                 spritectx.fillStyle = color;                
                 spritectx.fillRect(cx, cy, cw, pixh);
             }
@@ -72,7 +69,7 @@ function regenText(spritecanvas,spritectx) {
                 if (font.hasOwnProperty(n)) {
                     fontstr = font[n].split('\n').map(a=>a.trim().split('').map(t=>parseInt(t)));
                     fontstr.shift();
-                    textImages[n] = createSprite('char'+n,fontstr, undefined, 1);
+                    textImages[n] = createSprite('char'+n,fontstr, ["#00000000", state.fgcolor], 1);
                 } else {
                     //the character was not found, so
                     textImages[n] = renderCJKCharacter(n,1)